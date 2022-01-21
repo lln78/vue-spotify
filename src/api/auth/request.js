921e461dded78d9ffdb1ca7a781e59321dbd0e99
lln78 <llauden@laposte.net>
2022-01-21 16:47:33 +0100

    UPG : Force dev mode

--- src/api/auth/request.js
+++ src/api/auth/request.js
@@ -3,9 +3,7 @@ import axios from "axios";
 //@todo move baseURL to config file
 
 const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://spotify-lln-auth.herokuapp.com/"
-    : "http://localhost:4040/";
+  process.env.NODE_ENV = "https://spotify-lln-auth.herokuapp.com/";
 
 const request = axios.create({
   baseURL
